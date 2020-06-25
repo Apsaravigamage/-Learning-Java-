d4bd436e1ab937f6b1488973b71a5bc8575e6cb5
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-25 18:24:25 +0530

    Variables in java

---
+++ variable.java
@@ -0,0 +1,15 @@
+class addVariable{
+    public static void main(String a[])
+    {
+        int num1 = 6;
+        int num2 = 6;
+        System.out.println(num1+num2);
+
+        float n1 = 12.6f;//4 bytes 
+        float n2 = 2.2f;
+        System.out.println(n1+n2);
+
+        char c ='A';
+        System.out.println((int)c); //ASCII value of character
+    }
+}
