b8c41fb9b0774beffcf6a88ea725b5098fdf746e
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-26 21:22:53 +0530

    Binary Literal in java

---
+++ binaryLiteral.java
@@ -0,0 +1,10 @@
+public class binaryLiteral{
+    public static void main (String a[])
+    {
+        int i = 0B101;
+        System.out.println(i);
+
+        int j = 0B100_00_00_00_00;
+        System.out.println(j);
+    }
+}
