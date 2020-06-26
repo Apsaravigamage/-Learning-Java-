7637fd34274c99ae31006415a3b0daa5fad87c6f
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-26 10:58:51 +0530

    Printf method in Java

---
+++ printf.java
@@ -0,0 +1,13 @@
+public class printf{
+    public static void main (String a[])
+    {
+        int i,j,k;
+        i=5;
+        j=6;
+        k=i+j;
+        System.out.println("The addition of "+i +" and "+j +" is "+k);
+        System.out.printf("The addition of %d and %d is %d",i,j,k);
+
+
+    }
+}
