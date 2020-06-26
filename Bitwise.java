4fd5ee49d8919e4d71305919ed1911e6b0405981
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-26 22:51:13 +0530

    Bitwise And & Or operator in Java

---
+++ Bitwise.java
@@ -0,0 +1,14 @@
+public class Bitwise{
+    public static void main (String a[])
+    {
+        // Bitwise operator AND OR
+       int i = 25;
+       int b = 15;
+
+       int c = i & b;
+       System.out.println(c); 
+
+       int q = i | b;
+       System.out.println(q);
+    }
+}
