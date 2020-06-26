d5e081fafc347ea44f6503f2f41b0711fe30687f
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-26 22:28:32 +0530

    post and pre increment in Java

---
+++ increment.java
@@ -0,0 +1,10 @@
+public class increment{
+    public static void main (String a[])
+    {
+        int i = 5;
+        //int j = i++; // post increment
+        int k = ++i; // pre increment
+        //System.out.println(j);
+        System.out.println(k);
+    }
+}
