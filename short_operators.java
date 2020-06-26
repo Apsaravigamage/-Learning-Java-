dbda451f0b01da93274999837af3b99f02a51706
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-26 21:11:43 +0530

    Short Hand Operators in java

---
+++ short_operators.java
@@ -0,0 +1,19 @@
+public class short_operators{
+    public static void main(String a[])
+    {
+       //short hand operators +=, -=, *= ...
+       int i=5;
+
+       i += 2; //i = i + 2
+       System.out.println(i);
+
+        i -= 2; //i = i - 2
+       System.out.println(i);
+
+        i *= 2; //i = i * 2
+       System.out.println(i);
+
+        i /= 2; //i = i / 2
+       System.out.println(i);
+    }
+}
