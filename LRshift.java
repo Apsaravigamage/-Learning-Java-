41c9083fe5c341f39decc9d82783161ce11c8e4c
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-28 23:00:08 +0530

    Left & Right Shift in Java

---
+++ LRshift.java
@@ -0,0 +1,14 @@
+ public class LRshift{
+    public static void main(String a[])
+    {
+        int c = 8; //1000
+        int b = c << 2; //100000.  leftshift
+        System.out.println(b);
+
+        
+        int x = 8; //1000
+        int z = x >> 2; //10.  Rightshift
+        System.out.println(z);
+    }
+
+}
