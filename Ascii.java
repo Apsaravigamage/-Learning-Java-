5f06cea8c9f132450b197f65be5abc84f617bbdc
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-29 20:43:05 +0530

    Print ASCII values in Java

---
+++ Ascii.java
@@ -0,0 +1,13 @@
+public class Ascii{
+    public static void main(String a[])
+    {
+        // 7 bits
+        // 2^7 = 128
+        // 0-127
+        for(int i=0; i<=127; i++)
+        {
+            System.out.printf("%d : %c \n",i,i);
+        }
+        
+    }
+}
