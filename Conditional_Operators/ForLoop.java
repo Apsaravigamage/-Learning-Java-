bf68ee8203bf6b5ad0266fb2b8710089b2a0ecff
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-29 19:42:19 +0530

    For & While Loop in Java

---
+++ Conditional_Operators/ForLoop.java
@@ -0,0 +1,28 @@
+public class ForLoop{
+    public static void main(String a[])
+    {
+        //While
+        int i =1;
+        while(i<=10)
+        {
+            System.out.println(i);
+            i++;
+        }
+
+        // Do While
+        int x=1;
+        do
+        {
+            System.out.println(x);
+            x++;
+        }while(x<10);
+
+        //For
+        for(int n=1;n<=10;n++)
+        {
+            System.out.println(n);
+        }
+
+
+    }
+}
