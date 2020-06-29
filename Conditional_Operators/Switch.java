f03032aa9dd682c1fb0f5c8be072054e6b63eb0c
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-29 18:12:38 +0530

    Switch case in Java

---
+++ Conditional_Operators/Switch.java
@@ -0,0 +1,23 @@
+public class Switch{
+    public static void main(String a[])
+    {
+        int i=0;
+
+        switch(i)
+        {
+            case 0:
+                 System.out.println("0");
+                 break;
+            case 1:
+                 System.out.println("1");
+                 break;
+            case 5:
+                 System.out.println("5");
+                 break;
+            default:
+                 System.out.println("some number");
+
+        }
+       
+    }
+}
