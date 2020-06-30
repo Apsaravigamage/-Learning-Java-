dc3ae475826852166cd0bfad6680c1d3e747cfc6
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-06-30 11:41:09 +0530

    Palindrome Number in Java

---
+++ Palindrome.java
@@ -0,0 +1,24 @@
+public class Palindrome{
+    public static void main(String a[])
+    {
+        int n=121 , r , s=0;
+        int t=n;
+
+        while(n>0)
+        {
+            r=n%10;
+            n=n/10;
+            s=s*10+r;
+        }
+        if(t==s)
+        {
+            System.out.println("Palindrome Number");
+        }
+        else
+        {
+            System.out.println("Not Palindrome Number");
+        }
+
+        
+    }
+}
